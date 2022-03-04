bbab5e030bda6623214d17cf35e771dcbc8ebcfd
Drew Sommer <drew.sommer@sulzer-us.com>
2022-03-04 13:26:16 -0500

    feat: Order plants list in two sections

--- advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -17,6 +17,7 @@
 package com.example.android.advancedcoroutines
 
 import com.example.android.advancedcoroutines.util.CacheOnSuccess
+import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
@@ -97,7 +98,14 @@ class PlantRepository private constructor(
             plantService.customPlantSortOrder()
         }
 
-
+    private fun List<Plant>.applySort(customSortOrder: List<String>): List<Plant> {
+        return sortedBy { plant ->
+            val positionForItem = customSortOrder.indexOf(plant.plantId).let { order ->
+                if (order > -1) order else Int.MAX_VALUE
+            }
+            ComparablePair(positionForItem, plant.name)
+        }
+    }
 
     companion object {
 
